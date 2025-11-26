dfa30577df561f0085ad25a3d8836c76587e3459
kavyashah3011 <error.in.alone@gmail.com>
2025-11-26 21:48:50 +0530

    commit

---
+++ task1.py
@@ -0,0 +1,23 @@
+"""Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Performing basic mathematical operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
