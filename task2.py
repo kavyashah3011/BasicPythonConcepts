d5c804fdc779a14b536f9b6552a84be0bb2d8747
kavyashah3011 <error.in.alone@gmail.com>
2025-11-26 22:02:29 +0530

    task2

---
+++ task2.py
@@ -0,0 +1,11 @@
+"""Task 2: Create a Personalized Greeting Problem Statement: Write a Python program that:
+
+Takes a user's first name and last name as input.
+Concatenates the first name and last name into a full name.
+Prints a personalized greeting message using the full name.
+"""
+
+name1 = input("Enter your first name: ")
+name2 = input("Enter your last name: ")
+
+print("Hello, " + name1 + " " + name2 + "! Welcome aboard!")
